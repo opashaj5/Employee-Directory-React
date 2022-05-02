532953497659980aa3b46572b4066dddb62516e5
opashaj5 <opashaj5@yahoo.com>
2022-05-02 17:47:25 -0500

    update data.js

--- src/data.js
+++ src/data.js
@@ -2,36 +2,60 @@ export default [
     {
         img: "https://home.adelphi.edu/~ni21572/Monica.jpg",
         name: "Monica Geller",
-        title: "Chef"
+        title: "Chef",
+        office: "781-000-0001",
+        mobile: "617-000-0001",
+        SMS: "617-000-0001",
+        email: "monica@fakeemail.com"
     },
     {
         img:
             "https://pyxis.nymag.com/v1/imgs/47c/71a/130bf1e557e534b3f2be3351afc2ecf952-17-rachel-green-jewish.rsquare.w700.jpg",
         name: "Rachel Green",
-        title: "Fashion Executive"
+        title: "Fashion Executive",
+        office: "781-000-0002",
+        mobile: "617-000-0002",
+        SMS: "617-000-0002",
+        email: "rachel@fakeemail.com"
     },
     {
         img:
             "https://media1.popsugar-assets.com/files/thumbor/Jag4QheIiSRDyPige1c7d6J7X68/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/07/25/859/n/1922398/1bb1be5259779df9303e84.02388163_edit_img_image_43785815_1500923262/i/Phoebe-Buffay-Quotes-From-Friends.jpg",
         name: "Phoebe Buffay",
-        title: "Masseuse/Musician"
+        title: "Masseuse/Musician",
+        office: "781-000-0002",
+        mobile: "617-000-0002",
+        SMS: "617-000-0002",
+        email: "phoebe@fakeemail.com"
     },
     {
         img:
             "https://fotografias.antena3.com/clipping/cmsimages02/2017/08/22/17C10B2A-7B04-4686-A3F5-959ACD199DB2/69.jpg?crop=1:1,smart&width=1200&height=1200&optimize=low&format=webply",
         name: "Ross Geller",
-        title: "Paleontologist/Professor"
+        title: "Paleontologist/Professor",
+        office: "781-000-0004",
+        mobile: "617-000-0004",
+        SMS: "617-000-0004",
+        email: "ross@fakeemail.com"
     },
     {
         img:
             "https://i.guim.co.uk/img/media/513976d50736695ee8bd5014175e007f9980531f/0_289_2980_1788/master/2980.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=77863ec7b2622437668af3ff9ecd7ae6",
         name: "Joey Tribbiani",
-        title: "Actor"
+        title: "Actor",
+        office: "781-000-0005",
+        mobile: "617-000-0005",
+        SMS: "617-000-0005",
+        email: "joey@fakeemail.com"
     },
     {
         img:
             "https://www.estrelando.com.br/uploads/2017/03/31/chandler-1490986209.jpg",
         name: "Chandler Bing",
-        title: "IT Procurement/Advertising Copywriter"
+        title: "IT Procurement/Advertising Copywriter",
+        office: "781-000-0006",
+        mobile: "617-000-0006",
+        SMS: "617-000-0006",
+        email: "chandler@fakeemail.com"
     }
 ];
